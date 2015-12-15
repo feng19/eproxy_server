77765acafaadebde369e2550ebbc380c113c0b14
little-f <lo.ve19@qq.com>
2015-12-15 14:47:35 +0800

    hidden debug

--- include/debug.hrl
+++ include/debug.hrl
@@ -1,4 +1,3 @@
 
--define(DEBUG(Msg),io:format("[~p:~p] ~p~n",[?MODULE,?LINE,Msg])).
-
--define(DEBUG(Format,Args),io:format("[~p:~p] "++Format++"~n",[?MODULE,?LINE|Args])).
+-define(DEBUG(Msg),make_proxy_server:debug(Msg)).
+-define(DEBUG(Format,Args),make_proxy_server:debug(Format,Args)).
